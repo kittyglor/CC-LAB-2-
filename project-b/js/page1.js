609090b9326a473c6963fc6f64c47b08efb1a60f
kittyglor <glorias1818@yahoo.com>
2024-12-06 14:07:35 +0800

    bears
    uploading draft of project b

---
+++ project-b/js/page1.js
@@ -0,0 +1,153 @@
+const grid = document.querySelector('.grid');
+    const imageUrl = 'tb1.png'; // Replace with your image URL
+
+    for (let i = 0; i < 20_000; i++) {
+      const img = document.createElement('img');
+      img.src = imageUrl;
+      img.alt = 'Teddy Bear';
+      grid.appendChild(img);
+
+    }
+
+
+
+
+
+
+
+
+
+
+// let soundFiles = [
+//   "assets/beat.mp3",
+//   "assets/kick.mp3",
+//   // feel free to add more!
+// ];
+// let sounds = [];
+// let particles = [];
+
+// function preload() {
+//   for (let i = 0; i < soundFiles.length; i++) {
+//     let eachSound = loadSound(soundFiles[i]);
+//     sounds.push(eachSound);
+//   }
+// }
+
+// function setup() {
+//   let canvas = createCanvas(800, 500);
+//   canvas.parent("p5-canvas-container");
+//   background(220);
+// }
+
+// function draw() {
+//   background(220);
+
+//   // generate a particle with a 3% chance
+//   if (random() < 0.03) {
+//     let sound = random(sounds); // pick a random sound from the array
+//     particles.push(new Particle(random(width), random(height), random(50, 80), sound));
+//   }
+
+//   // update, compare and display
+//   for (let i = 0; i < particles.length; i++) {
+//     let p = particles[i];
+//     p.move();
+//     p.adjustSpeed(-0.02);
+//     //p.fall();
+//     p.age();
+//     p.checkMouse();
+//     p.display();
+//   }
+
+//   // remove a particle if it's done!
+//   // ensure you flip the loop!!!
+//   for (let i = particles.length - 1; i >= 0; i--) {
+//     let p = particles[i]; // each element
+//     if (p.isDone == true) {
+//       particles.splice(i, 1); // (index, quantity)
+//     }
+//   }
+
+//   // limit the total number
+//   while (particles.length > 500) {
+//     particles.splice(0, 1); // (index, quantity)
+//   }
+// }
+
+
+// class Particle {
+//   constructor(x, y, rad, sound) {
+//     this.sound = sound;
+//     this.soundRate = random(0.50, 2.00);
+
+//     this.x = x;
+//     this.y = y;
+//     this.xSpeed = random(-3, 3);
+//     this.ySpeed = random(-3, 3);
+//     this.rad = rad;
+
+//     this.r = 255;
+//     this.g = 255;
+//     this.b = 255;
+
+//     this.lifespan = 1.0; // 100%
+//     this.lifeReduction = 0.01; // 1%
+//     this.isDone = false;
+//   }
+//   display() {
+//     push();
+//     translate(this.x, this.y);
+
+//     noStroke();
+//     // fill(this.r, this.g, this.b, 255 * this.lifespan); // reduce the alpha by the lifespan  
+//     fill(this.r, this.g, this.b);
+//     circle(0, 0, this.rad * 2 * this.lifespan); // reduce the size by the lifespan
+
+//     pop();
+//   }
+//   move() {
+//     this.x += this.xSpeed;
+//     this.y += this.ySpeed;
+//   }
+//   fall() {
+//     this.ySpeed += 0.1;
+//   }
+//   adjustSpeed(amount = 0) {
+//     this.xSpeed = this.xSpeed * (1 + amount);
+//     this.ySpeed = this.ySpeed * (1 + amount);
+//   }
+//   age() {
+//     if (this.lifespan > 0) {
+//       this.lifespan -= this.lifeReduction
+//     } else {
+//       this.lifespan = 0;
+//       this.isDone = true;
+//     }
+//   }
+//   checkMouse() {
+//     let distance = dist(this.x, this.y, mouseX, mouseY);
+//     if (distance < this.rad) {
+//       // in
+//       this.r = 255;
+//       this.g = 255;
+//       this.b = 0; // yellow
+
+//       if (mouseIsPressed) {
+//         // change the color
+//         this.r = 255;
+//         this.g = 0;
+//         this.b = 0; // red 
+//         this.isDone = true;
+
+//         // play the sound
+//         this.sound.rate(this.soundRate);
+//         this.sound.play();
+//       }
+//     } else {
+//       // out
+//       this.r = 255;
+//       this.g = 255;
+//       this.b = 255; // white
+//     }
+//   }
+// }
---
+++ project-b/js/page2.js
@@ -0,0 +1,49 @@
+// let sound;
+
+// function preload() {
+//   // Load the sound file
+//   sound = loadSound('../assets/beat.mp3'); // Replace with your sound file
+// }
+
+// function setup() {
+//   noCanvas(); // No canvas needed if you're just playing a sound
+//   sound.play(); // Play the sound when the page loads
+// }
+
+
+
+
+
+
+let sound;
+
+function preload() {
+  sound = loadSound('../assets/audio.mp3'); // Replace with your sound file
+}
+
+function setup() {
+  noCanvas();
+
+  // Attempt sound playback after a short delay
+  getAudioContext().resume().then(() => {
+    setTimeout(() => {
+      sound.play();
+    }, 1000); // Adjust delay if needed
+  }).catch(err => {
+    console.log("Autoplay blocked until user interaction:", err);
+  });
+}
+
+
+
+// function setup() {
+//   let canvas = createCanvas(800, 500);
+//   canvas.parent("p5-canvas-container");
+//   background(220);
+// }
+
+// function draw() {
+//   noStroke();
+//   fill(random(255), random(255), random(255));
+//   circle(random(width), random(height), random(50, 80));
+// }
